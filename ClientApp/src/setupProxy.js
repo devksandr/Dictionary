e74f748663815eae815c52701beb6f4a6cf59f1a
testuser <testemail>
2023-03-19 20:13:48 +0700

    BF: Added swagger

--- ClientApp/src/setupProxy.js
+++ ClientApp/src/setupProxy.js
@@ -6,7 +6,8 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 
 const context = [
   "/weatherforecast",
-  "/api/files"
+  "/api/files",
+  "/swagger"
 ];
 
 const onError = (err, req, resp, target) => {
--- Program.cs
+++ Program.cs
@@ -3,10 +3,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationContext>();
 builder.Services.AddControllersWithViews();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
