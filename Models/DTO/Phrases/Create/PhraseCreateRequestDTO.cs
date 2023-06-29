74864921a8354dbf1b39ae679ec63ef152964a50
Ksandr <MYEMAIL>
2023-06-29 21:17:44 +0700

    B: Added validation for UpdatePhrase model

--- Models/DTO/Phrases/Create/PhraseCreateRequestDTO.cs
+++ Models/DTO/Phrases/Create/PhraseCreateRequestDTO.cs
@@ -1,5 +1,4 @@
 using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNetCore.Mvc;
 namespace dict_react.Models.DTO;
 
 public class PhraseCreateRequestDTO
--- Models/DTO/Phrases/Update/PhraseUpdateRequestDTO.cs
+++ Models/DTO/Phrases/Update/PhraseUpdateRequestDTO.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 namespace dict_react.Models.DTO;
 
 public class PhraseUpdateRequestDTO
@@ -6,7 +6,10 @@ public class PhraseUpdateRequestDTO
     public int PhraseId { get; set; }
     public int PhraseMeaningId { get; set; }
     public int CategoryId { get; set; }
+    [MinLength(3)]
+    [Required]
     public string? Phrase { get; set; }
+    [Required]
     public string? Meaning { get; set; }
     public string? Comment { get; set; }
 }
